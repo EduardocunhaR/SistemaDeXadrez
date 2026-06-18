dd4e431a3fbaee01c12a841318e9dead285e1a90
Eduardo Cunha Ribeiro <eduzinhobrp@gmail.com>
2026-06-18 16:45:08 -0300

    Finalizacao do projeto e adicao da documentacao

---
+++ application/Program.java
@@ -0,0 +1,62 @@
+package application;
+
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ChessMatch match = new ChessMatch();
+        List<ChessPiece> capturedPieces = new ArrayList<>();
+
+        while (!match.getCheckMate()) {
+            try {
+                UI.clearScreen();
+                UI.printMatch(match, capturedPieces);
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                
+                boolean[][] possibleMoves = match.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(match.getPieces(), possibleMoves);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = match.performChessMove(source, target);
+
+                if (capturedPiece != null)
+                    capturedPieces.add(capturedPiece);
+
+                if (match.getPromoted() != null) {
+                    System.out.print("To wich piece you want to promote (B/N/Q/R)? ");
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("Q") && !type.equals("R")) {
+                        System.out.print("Invalid choice. Enter piece for promotion (B/N/Q/R): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+                    match.replacePromotedPiece(type);
+                }
+            }
+            catch (ChessException exception) {
+                System.out.println(exception.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException exception) {
+                System.out.println(exception.getMessage());
+                sc.nextLine();
+            }
+        }
+        UI.clearScreen();
+        UI.printMatch(match, capturedPieces);
+
+        sc.close();
+    }
+}
